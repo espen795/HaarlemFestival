bf3ca0c085f5d00724a191526f78d1bf0e78f425
Patrick <577234@student.inholland.nl>
2017-11-10 17:45:32 +0100

    LoginModel aangemaakt voor het inloggen van de admins

---
+++ HaarlemFestival/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HaarlemFestival.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Username: ")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password: ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
