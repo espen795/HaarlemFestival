1ae85b82604b5dc898daa6607d3cbc4c9e91ed6b
Patrick <577234@student.inholland.nl>
2017-11-15 12:11:40 +0100

    View resultaten aangemaakt

--- HaarlemFestival/Controllers/AdminController.cs
+++ HaarlemFestival/Controllers/AdminController.cs
@@ -3,12 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HaarlemFestival.Models;
 
 namespace HaarlemFestival.Controllers
 {
     public class AdminController : Controller
     {
-        public ActionResult Index()
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Username,Password")] LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Account account = new Account();
+
+                if (account != null) // Als het account bestaat
+                {
+                    FormsAuthentication.SetAuthCookie(account.Username, false);
+
+                    //Session["loggedin_account"] = account;
+
+                    return RedirectToAction("Overview", "Admin"); // De gebruiker naar zijn of haar contactenlijst sturen
+                }
+                else // Als het account niet bestaat
+                {
+                    ModelState.AddModelError("login-error", "Some of the entered information is invalid.");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+        // Uitloggen
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Session["loggedin_account"] = null; // Account uit de sessie verwijderen.
+            return RedirectToAction("Login", "Account"); // Gebruiker naar de inlogpagina sturen.
+        }
+
+        public ActionResult ManageEvent()
         {
             return View();
         }
