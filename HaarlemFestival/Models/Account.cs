eb65f008734add11ae4f8716886ef8bcb4190303
Patrick <577234@student.inholland.nl>
2017-11-15 12:18:46 +0100

    Account Model aangemaakt

---
+++ HaarlemFestival/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
