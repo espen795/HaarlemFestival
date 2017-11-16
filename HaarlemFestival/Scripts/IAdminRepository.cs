6569a24628b50e69b99f162c104bf0b35c047fdf
Patrick <577234@student.inholland.nl>
2017-11-16 13:25:13 +0100

    Interface voor adminRepository aangemaakt en standaard functies toegevoegd (W.I.P)

---
+++ HaarlemFestival/Scripts/IAdminRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HaarlemFestival.Models;
+
+namespace HaarlemFestival.Scripts
+{
+    interface IAdminRepository
+    {
+        // Login
+        Account GetAccount();
+
+        // Manage Events
+        void UpdateEvent();
+        void DeleteEvent();
+        EventList GetEvents();
+    }
+}
