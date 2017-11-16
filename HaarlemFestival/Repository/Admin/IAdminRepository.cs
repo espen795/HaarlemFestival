bf6c7ff41d2c29b61da9ed7d08b86e61a57f11dd
Patrick <577234@student.inholland.nl>
2017-11-16 14:08:47 +0100

    Admin Interface toegevoegd

---
+++ HaarlemFestival/Repository/Admin/IAdminRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HaarlemFestival.Models;
+
+namespace HaarlemFestival.Repository.Admin
+{
+    interface IAdminRepository
+    {
+        // Login
+        Account GetAccount();
+
+        // ManageEvent
+        void UpdateEvent();
+        void DeleteEvent();
+        EventList GetEvents();
+    }
+}
