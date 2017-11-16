f0b795e1fbdb45691acdaf1faf926043be7fdda5
Patrick <577234@student.inholland.nl>
2017-11-16 13:16:51 +0100

    Models voor evenementen aangemaakt ( Nog leeg, geen gegevens )

---
+++ HaarlemFestival/Models/EventList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Models
+{
+    public class EventList
+    {
+        List<Jazz> Bands;
+        List<Dinner> Restaurants;
+        List<Talking> Interviews;
+        List<Historic> Tours;
+    }
+}
