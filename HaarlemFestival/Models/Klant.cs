35d7116b843d3a737de3876b5998e330391b088b
Bas Tolen <bastolen97@gmail.com>
2017-11-17 13:46:46 +0100

    Hello guys its ya boiii anomaly

---
+++ HaarlemFestival/Models/Klant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Models
+{
+    public class Klant
+    {
+        public int KlantId { get; set; }
+        public string Naam { get; set; }
+        public string Email { get; set; }
+        public string BetaalMethode { get; set; }
+    }
+}
