16c82797689af1528fbc079cb13ad0256e946260
Bas Tolen <bastolen97@gmail.com>
2017-11-23 13:23:04 +0100

    constructor activity toegevoegd

--- HaarlemFestival/Models/Activity.cs
+++ HaarlemFestival/Models/Activity.cs
@@ -16,5 +16,17 @@ namespace HaarlemFestival.Models
         public int BoughtTickets { get; set; }
         public string Phonenumber { get; set; }
         public string Address { get; set; }
+
+        public Activity(EventType eventType, DateTime dateReservation, DateTime startSession, DateTime endSession, int totalTickets, int boughtTickets, string phonenumber, string address)
+        {
+            EventType = eventType;
+            DateReservation = dateReservation;
+            StartSession = startSession;
+            EndSession = endSession;
+            TotalTickets = totalTickets;
+            BoughtTickets = boughtTickets;
+            Phonenumber = phonenumber;
+            Address = address;
+        }
     }
 }
--- HaarlemFestival/Models/Dinner.cs
+++ HaarlemFestival/Models/Dinner.cs
@@ -7,6 +7,14 @@ namespace HaarlemFestival.Models
 {
     public class Dinner : Activity
     {
+        public int RestaurantId { get; set; }
         public virtual Restaurant Restaurant { get; set; }
+
+        public Dinner(EventType eventType, DateTime dateReservation, DateTime startSession, DateTime endSession, int totalTickets, int boughtTickets, string phonenumber, string address, int restaurantid, Restaurant restaurant) : 
+            base(eventType, dateReservation, startSession, endSession, totalTickets, boughtTickets, phonenumber, address)
+        {
+            RestaurantId = restaurantid;
+            Restaurant = restaurant;
+        }
     }
 }
