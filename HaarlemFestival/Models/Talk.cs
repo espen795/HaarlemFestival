0e7aa36d18d7985c9c584427eb019cbcb9e4b36d
giannistek1 <gianni.stekkinger@hotmail.nl>
2017-11-27 14:35:18 +0100

    Models talking toegevoegd

---
+++ HaarlemFestival/Models/Talk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Models
+{
+    public class Talk
+    {
+        public string gesprekspersoon1;
+        public string gesprekspersoon2;
+        public Interviewvraag vraag;
+        public int maxTickets;
+        
+        public Talk()
+        {
+
+        }
+    }
+}
