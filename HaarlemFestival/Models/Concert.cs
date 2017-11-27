0cc7a364f6b7d5b6a64ca2506e2ca1c37551e276
Kevin <Kevin@KEVINBOSMA7DE8>
2017-11-27 14:31:41 +0100

    Jazz Model

---
+++ HaarlemFestival/Models/Concert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFestival.Models
+{
+    public class Concert
+    {
+        public int JazzId { get; set; }
+        public int RegularTicketAmount { get; set; }
+        public int PassPartoutTicketAmount { get; set; }
+        public float TotalPrice { get; set; }
+        public DateTime Day { get; set; }
+        public string Hall { get; set; }
+
+        public Concert()
+        {
+
+        }
+    }
+}
