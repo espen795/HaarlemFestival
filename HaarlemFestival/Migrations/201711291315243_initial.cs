925b1e332740ec10e5e33ec042b262f508e81274
Bas Tolen <bastolen97@gmail.com>
2017-11-29 14:18:22 +0100

    DataBaseCorrect #YAY #WEDIDIT #MAKEAMERICAGREATAGAIN #ESPENLOVESSANDPAPERINGFRIENDS

--- HaarlemFestival/Migrations/201711291315243_initial.cs
+++ HaarlemFestival/Migrations/201711291315243_initial.cs
@@ -19,6 +19,34 @@ namespace HaarlemFestival.Migrations
                 .PrimaryKey(t => t.Id);
             
             CreateTable(
+                "dbo.Artists",
+                c => new
+                    {
+                        ArtistId = c.Int(nullable: false, identity: true),
+                        ArtistName = c.String(),
+                        ArtistImage = c.String(),
+                        ArtistInformation = c.String(),
+                    })
+                .PrimaryKey(t => t.ArtistId);
+            
+            CreateTable(
+                "dbo.BesteldeActiviteits",
+                c => new
+                    {
+                        BesteldeActiviteitId = c.Int(nullable: false, identity: true),
+                        ReserveringId = c.Int(nullable: false),
+                        Aantal = c.Int(nullable: false),
+                        Opmerking = c.String(),
+                        ActiviteitId = c.Int(nullable: false),
+                        Activiteit_ActivityId = c.Int(),
+                    })
+                .PrimaryKey(t => t.BesteldeActiviteitId)
+                .ForeignKey("dbo.Activities", t => t.Activiteit_ActivityId)
+                .ForeignKey("dbo.Reserverings", t => t.ReserveringId, cascadeDelete: true)
+                .Index(t => t.ReserveringId)
+                .Index(t => t.Activiteit_ActivityId);
+            
+            CreateTable(
                 "dbo.Activities",
                 c => new
                     {
@@ -33,24 +61,8 @@ namespace HaarlemFestival.Migrations
                         BoughtTickets = c.Int(nullable: false),
                         Phonenumber = c.String(),
                         Address = c.String(),
-                        RestaurantId = c.Int(),
-                        TourId = c.Int(),
-                        ConcertLocation = c.String(),
-                        ConcertHall = c.String(),
-                        AllDayPassPartout = c.Single(),
-                        ArtistId = c.Int(),
-                        Discriminator = c.String(nullable: false, maxLength: 128),
-                        Talk_TalkId = c.Int(),
                     })
-                .PrimaryKey(t => t.ActivityId)
-                .ForeignKey("dbo.Restaurants", t => t.RestaurantId, cascadeDelete: true)
-                .ForeignKey("dbo.Tours", t => t.TourId, cascadeDelete: true)
-                .ForeignKey("dbo.Artists", t => t.ArtistId, cascadeDelete: true)
-                .ForeignKey("dbo.Talks", t => t.Talk_TalkId)
-                .Index(t => t.RestaurantId)
-                .Index(t => t.TourId)
-                .Index(t => t.ArtistId)
-                .Index(t => t.Talk_TalkId);
+                .PrimaryKey(t => t.ActivityId);
             
             CreateTable(
                 "dbo.Restaurants",
@@ -112,40 +124,12 @@ namespace HaarlemFestival.Migrations
                 .PrimaryKey(t => t.LanguageId);
             
             CreateTable(
-                "dbo.Artists",
-                c => new
-                    {
-                        ArtistId = c.Int(nullable: false, identity: true),
-                        ArtistName = c.String(),
-                        ArtistImage = c.String(),
-                        ArtistInformation = c.String(),
-                    })
-                .PrimaryKey(t => t.ArtistId);
-            
-            CreateTable(
                 "dbo.Talks",
                 c => new
                     {
                         TalkId = c.Int(nullable: false, identity: true),
                     })
                 .PrimaryKey(t => t.TalkId);
-            
-            CreateTable(
-                "dbo.BesteldeActiviteits",
-                c => new
-                    {
-                        BesteldeActiviteitId = c.Int(nullable: false, identity: true),
-                        ReserveringId = c.Int(nullable: false),
-                        Aantal = c.Int(nullable: false),
-                        Opmerking = c.String(),
-                        ActiviteitId = c.Int(nullable: false),
-                        Activiteit_ActivityId = c.Int(),
-                    })
-                .PrimaryKey(t => t.BesteldeActiviteitId)
-                .ForeignKey("dbo.Activities", t => t.Activiteit_ActivityId)
-                .ForeignKey("dbo.Reserverings", t => t.ReserveringId, cascadeDelete: true)
-                .Index(t => t.ReserveringId)
-                .Index(t => t.Activiteit_ActivityId);
             
             CreateTable(
                 "dbo.Reserverings",
@@ -188,43 +172,110 @@ namespace HaarlemFestival.Migrations
                     })
                 .PrimaryKey(t => t.InterviewQuestionId);
             
+            CreateTable(
+                "dbo.Jazzs",
+                c => new
+                    {
+                        ActivityId = c.Int(nullable: false),
+                        ConcertLocation = c.String(),
+                        ConcertHall = c.String(),
+                        AllDayPassPartout = c.Single(nullable: false),
+                        ArtistId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ActivityId)
+                .ForeignKey("dbo.Activities", t => t.ActivityId)
+                .ForeignKey("dbo.Artists", t => t.ArtistId, cascadeDelete: true)
+                .Index(t => t.ActivityId)
+                .Index(t => t.ArtistId);
+            
+            CreateTable(
+                "dbo.Dinners",
+                c => new
+                    {
+                        ActivityId = c.Int(nullable: false),
+                        RestaurantId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ActivityId)
+                .ForeignKey("dbo.Activities", t => t.ActivityId)
+                .ForeignKey("dbo.Restaurants", t => t.RestaurantId, cascadeDelete: true)
+                .Index(t => t.ActivityId)
+                .Index(t => t.RestaurantId);
+            
+            CreateTable(
+                "dbo.Historics",
+                c => new
+                    {
+                        ActivityId = c.Int(nullable: false),
+                        TourId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ActivityId)
+                .ForeignKey("dbo.Activities", t => t.ActivityId)
+                .ForeignKey("dbo.Tours", t => t.TourId, cascadeDelete: true)
+                .Index(t => t.ActivityId)
+                .Index(t => t.TourId);
+            
+            CreateTable(
+                "dbo.Talkings",
+                c => new
+                    {
+                        ActivityId = c.Int(nullable: false),
+                        Talk_TalkId = c.Int(),
+                    })
+                .PrimaryKey(t => t.ActivityId)
+                .ForeignKey("dbo.Activities", t => t.ActivityId)
+                .ForeignKey("dbo.Talks", t => t.Talk_TalkId)
+                .Index(t => t.ActivityId)
+                .Index(t => t.Talk_TalkId);
+            
         }
         
         public override void Down()
         {
+            DropForeignKey("dbo.Talkings", "Talk_TalkId", "dbo.Talks");
+            DropForeignKey("dbo.Talkings", "ActivityId", "dbo.Activities");
+            DropForeignKey("dbo.Historics", "TourId", "dbo.Tours");
+            DropForeignKey("dbo.Historics", "ActivityId", "dbo.Activities");
+            DropForeignKey("dbo.Dinners", "RestaurantId", "dbo.Restaurants");
+            DropForeignKey("dbo.Dinners", "ActivityId", "dbo.Activities");
+            DropForeignKey("dbo.Jazzs", "ArtistId", "dbo.Artists");
+            DropForeignKey("dbo.Jazzs", "ActivityId", "dbo.Activities");
             DropForeignKey("dbo.Reserverings", "KlantId", "dbo.Klants");
             DropForeignKey("dbo.BesteldeActiviteits", "ReserveringId", "dbo.Reserverings");
             DropForeignKey("dbo.BesteldeActiviteits", "Activiteit_ActivityId", "dbo.Activities");
-            DropForeignKey("dbo.Activities", "Talk_TalkId", "dbo.Talks");
-            DropForeignKey("dbo.Activities", "ArtistId", "dbo.Artists");
-            DropForeignKey("dbo.Activities", "TourId", "dbo.Tours");
             DropForeignKey("dbo.Tours", "Language_LanguageId", "dbo.Languages");
             DropForeignKey("dbo.Tours", "Guide_GuideId", "dbo.Guides");
-            DropForeignKey("dbo.Activities", "RestaurantId", "dbo.Restaurants");
             DropForeignKey("dbo.Cuisines", "Restaurant_RestaurantId", "dbo.Restaurants");
+            DropIndex("dbo.Talkings", new[] { "Talk_TalkId" });
+            DropIndex("dbo.Talkings", new[] { "ActivityId" });
+            DropIndex("dbo.Historics", new[] { "TourId" });
+            DropIndex("dbo.Historics", new[] { "ActivityId" });
+            DropIndex("dbo.Dinners", new[] { "RestaurantId" });
+            DropIndex("dbo.Dinners", new[] { "ActivityId" });
+            DropIndex("dbo.Jazzs", new[] { "ArtistId" });
+            DropIndex("dbo.Jazzs", new[] { "ActivityId" });
             DropIndex("dbo.Reserverings", new[] { "KlantId" });
-            DropIndex("dbo.BesteldeActiviteits", new[] { "Activiteit_ActivityId" });
-            DropIndex("dbo.BesteldeActiviteits", new[] { "ReserveringId" });
             DropIndex("dbo.Tours", new[] { "Language_LanguageId" });
             DropIndex("dbo.Tours", new[] { "Guide_GuideId" });
             DropIndex("dbo.Cuisines", new[] { "Restaurant_RestaurantId" });
-            DropIndex("dbo.Activities", new[] { "Talk_TalkId" });
-            DropIndex("dbo.Activities", new[] { "ArtistId" });
-            DropIndex("dbo.Activities", new[] { "TourId" });
-            DropIndex("dbo.Activities", new[] { "RestaurantId" });
+            DropIndex("dbo.BesteldeActiviteits", new[] { "Activiteit_ActivityId" });
+            DropIndex("dbo.BesteldeActiviteits", new[] { "ReserveringId" });
+            DropTable("dbo.Talkings");
+            DropTable("dbo.Historics");
+            DropTable("dbo.Dinners");
+            DropTable("dbo.Jazzs");
             DropTable("dbo.InterviewQuestions");
             DropTable("dbo.Days");
             DropTable("dbo.Klants");
             DropTable("dbo.Reserverings");
-            DropTable("dbo.BesteldeActiviteits");
             DropTable("dbo.Talks");
-            DropTable("dbo.Artists");
             DropTable("dbo.Languages");
             DropTable("dbo.Guides");
             DropTable("dbo.Tours");
             DropTable("dbo.Cuisines");
             DropTable("dbo.Restaurants");
             DropTable("dbo.Activities");
+            DropTable("dbo.BesteldeActiviteits");
+            DropTable("dbo.Artists");
             DropTable("dbo.Accounts");
         }
     }
