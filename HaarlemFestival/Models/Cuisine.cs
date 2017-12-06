2a26f63105e57bd45222ce94e0f7d19abe6476fc
Patrick <577234@student.inholland.nl>
2017-12-06 14:16:49 +0100

    Labels aan Models toegevoegd

--- HaarlemFestival/Models/Cuisine.cs
+++ HaarlemFestival/Models/Cuisine.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace HaarlemFestival.Models
 {
     public class Cuisine
     {
+        [Display(Name = "Cuisine Id")]
         public int CuisineId { get; set; }
+
+        [Display(Name = "Cuisine Naam")]
         public string Naam { get; set; }
         public virtual List<Restaurant> Restaurants { get; set; }
 
