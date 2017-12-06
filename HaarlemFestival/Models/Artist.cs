c04dcead394426b3bc963893afb5d510b704aff7
Patrick <577234@student.inholland.nl>
2017-12-06 14:10:14 +0100

    Labels aan een paar models gegeven, Partialview voor toevoegen data veranderd

--- HaarlemFestival/Models/Artist.cs
+++ HaarlemFestival/Models/Artist.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HaarlemFestival.Models
 {
     public class Artist
     {
+        
         public int ArtistId { get; set; }
+
+        [Display(Name = "Name")]
         public string ArtistName { get; set; }
 
+        [Display(Name = "Image")]
         public string ArtistImage { get; set; }
+
+        [Display(Name = "Description")]
         public string ArtistInformation { get; set; }
 
 
