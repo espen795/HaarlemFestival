8ca5826a5f592d6017df691cd714ef0b9d6d509f
Patrick <577234@student.inholland.nl>
2017-12-08 18:43:59 +0100

    Errormessage van Guide class veranderd.

--- HaarlemFestival/Models/Guide.cs
+++ HaarlemFestival/Models/Guide.cs
@@ -11,9 +11,11 @@ namespace HaarlemFestival.Models
         [Display(Name = "Guide Id")]
         public int GuideId { get; set; }
 
-        [Required(ErrorMessage = "Please enter the name of the guide.")]
+        [Required(ErrorMessage = "Please enter the guide's name.")]
         [Display(Name = "Guide Name")]
+
         public string GuideName { get; set; }
+        public DateTime WorksOn { get; set; }
 
         public Guide()
         {
