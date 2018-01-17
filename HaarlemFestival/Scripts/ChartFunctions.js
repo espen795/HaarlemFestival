893d7a97cc53b67367d8c5173dc49701e5f26ad2
Patrick <577234@student.inholland.nl>
2018-01-17 18:10:29 +0100

    Chart.Js Toegevoegd (Stijl van Charts en pagina moet nog gedaan worden)

---
+++ HaarlemFestival/Scripts/ChartFunctions.js
@@ -0,0 +1,55 @@
+﻿
+
+function CreateIncomeChart(id, jazzIncome, dinnerIncome, talkingIncome, historicIncome)
+{
+    var ctx = document.getElementById(id).getContext("2d");
+
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Jazz@Patronaat", "Dinner in Haarlem", "Talking Haarlem", "Historic Haarlem"],
+            datasets: [{
+                label: 'Income per event.',
+                data: [jazzIncome, dinnerIncome, talkingIncome, historicIncome],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
+
+function CreateTicketAvailabilityChart(id, availableTickets, soldTickets) {
+    var ctx = document.getElementById(id).getContext("2d");
+
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Available Tickets", "Sold Tickets"],
+            datasets: [{
+                label: 'Available tickets (spread over all events)',
+                data: [availableTickets, soldTickets],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
